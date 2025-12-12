f156204735e623ec78dd87cbf61ff69df0c444ff
leonardo160308 <lfarom2300@alumno.ipn.mx>
2025-12-11 23:37:03 -0600

    A

--- API/Backend/server.js
+++ API/Backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 
@@ -14,9 +13,6 @@ app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/productos", require("./routes/productRoutes"));
 app.use("/api/carrito", require("./routes/cartRoutes"));
 app.use("/api/venta", require("./routes/saleRoutes"));
-
-// Ruta de productos que venía del HEAD
-app.use("/", productroutes);
 
 // Levantar servidor
 const PORT = process.env.PORT || 3000;
